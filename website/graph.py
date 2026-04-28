478cdc5a8a248b09b06d771f68c92366c481d452
aishu <aismohan@iu.edu>
2026-04-28 17:31:30 -0400

    initial commit

--- website/graph.py
+++ website/graph.py
@@ -16,17 +16,36 @@ plt.savefig("etl_chart.png")
 plt.show()
 
 import matplotlib.pyplot as plt
+from collections import Counter
 
 
-labels = ['Success', 'Failure']
-values = [85, 15]
+years = [
+    2024, 2024, 2023,
+    2021, 2021, 2021, 2021, 2021, 2021, 2021,
+    2020, 2020, 2020, 2020, 2020,
+    2018, 2018, 2018, 2018, 2018,
+    2016, 2016, 2016,
+    2015, 2015,
+    2014, 2014
+]
+
+
+year_counts = Counter(years)
+
+sorted_years = sorted(year_counts.keys())
+counts = [year_counts[y] for y in sorted_years]
+
 
 plt.figure()
+plt.plot(sorted_years, counts, marker='o')
 
-plt.pie(values, labels=labels, autopct='%1.1f%%')
+plt.title("Procedures Over Time")
+plt.xlabel("Year")
+plt.ylabel("Number of Procedures")
 
-plt.title("API Request Success vs Failure")
+plt.grid()
 
-plt.tight_layout()
-plt.savefig("success_pie.png")  # saved image
+
+plt.savefig("procedure_chart.png", dpi=300, bbox_inches='tight')
+
 plt.show()
